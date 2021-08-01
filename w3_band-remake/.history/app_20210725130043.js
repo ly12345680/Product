624098e2c36fe24d5eb6b6da2e85878df85c339a
unknown <lynguyen4569@gmail.com>
2021-08-01 16:14:46 +0800

    add new brand W3school template

---
+++ w3_band-remake/.history/app_20210725130043.js
@@ -0,0 +1,40 @@
+const $= document.querySelector.bind(document);
+const $$= document.querySelectorAll.bind(document);
+const Btns= $$('.js-buy-tickets')
+const modal= $('.js-modal')
+const modalClose=$('.modal-close')
+const modalContainer= $('.js-modal-container')
+const header= $('#header')
+const moblieMenu=$('#moblie-menu')
+const headerHeight= header.clientHeight
+
+moblieMenu.onclick= function() {
+    const isClose= header.clientHeight===headerHeight;
+    if (isClose) {
+        header.style.height= 'auto'
+    } else {
+        header.style.height= null
+    }
+}
+
+
+
+
+
+
+function showBuyTickets() {
+    modal.classList.add('open')
+}
+function closeBuyTickets() {
+    modal.classList.remove('open')
+}
+
+
+for (const Btn of Btns) {
+    Btn.addEventListener('click', showBuyTickets)
+}
+modalClose.addEventListener('click', closeBuyTickets)
+modal.addEventListener('click', closeBuyTickets)
+modalContainer.addEventListener('click', function(event) {
+    event.stopPropagation()
+})
